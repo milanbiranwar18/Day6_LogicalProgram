3fd0fc604483b613575c496bfc849d9549a30bc3
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-03 16:42:32 +0530

    Adding Program for Fibonacci series

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@
         public static void Main(string[]arvg)
         {
             Console.WriteLine("Wellcome to Logical Programs");
+
+            LogicalProgram.FibonacciSeries();
         }
     }
 }
