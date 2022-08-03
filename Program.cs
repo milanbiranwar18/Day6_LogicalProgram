09e752f64436de6e79e90c129ad8ccd833a5344c
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-03 16:08:42 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Day6LogicalProgram
+{
+    public class Program
+    {
+
+        public static void Main(string[]arvg)
+        {
+            Console.WriteLine("Wellcome to Logical Programs");
+        }
+    }
+}
