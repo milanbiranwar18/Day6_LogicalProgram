b6f2ac546f999724f41745357c42fbb73adef697
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-05 15:43:41 +0530

    Program for StopWatch

---
+++ StopWatch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Day6LogicalProgram
+{
+    
+    public class StopWatch
+    {
+        public void Stopwatch()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            // Begin timing
+            stopwatch.Start();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Calculating time for your task");
+            }
+            // Stop timing
+            stopwatch.Stop();
+            Console.WriteLine("Time Elapsed : {0}",
+                stopwatch.Elapsed);
+            Console.ReadLine();
+        }
+    
+    }
+
+}
